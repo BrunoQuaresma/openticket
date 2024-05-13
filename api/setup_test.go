b322822bd31ec2d096d8f25248e4909e14886fae
Bruno Quaresma <bruno_nonato_quaresma@hotmail.com>
2024-05-13 21:16:24 +0000

    Restructure project layout II

--- api/setup_test.go
+++ api/setup_test.go
@@ -3,12 +3,12 @@ package api_test
 import (
 	"testing"
 
-	"github.com/BrunoQuaresma/openticket/api/test"
+	"github.com/BrunoQuaresma/openticket/api/testutil"
 	"github.com/stretchr/testify/require"
 )
 
 func TestSetup(t *testing.T) {
-	s := test.TestServer{}
+	s := testutil.TestServer{}
 	s.Start()
 	defer s.Close()
 	require.True(t, true)
