7d9a9fa328242c216b808645b3d124baaba21c18
Bruno Quaresma <bruno_nonato_quaresma@hotmail.com>
2024-06-19 22:41:34 +0000

    Refactor setup tests

--- api/setup_test.go
+++ api/setup_test.go
@@ -10,122 +10,113 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestSetup_Validation(t *testing.T) {
+func TestAPI_Setup(t *testing.T) {
 	t.Parallel()
 
-	tEnv := testutil.NewEnv(t)
-	tEnv.Start()
-	t.Cleanup(tEnv.Close)
+	t.Run("validation", func(t *testing.T) {
+		t.Parallel()
 
-	t.Run("required fields", func(t *testing.T) {
-		var (
-			req api.SetupRequest
-			res api.SetupResponse
-		)
-		sdk := tEnv.SDK()
-		httpRes, err := sdk.Setup(req, &res)
-		require.NoError(t, err, "error making request")
+		tEnv := testutil.NewEnv(t)
+		tEnv.Start()
+		t.Cleanup(tEnv.Close)
 
-		require.Equal(t, http.StatusBadRequest, httpRes.StatusCode)
-		testutil.RequireValidationError(t, res.Errors, "name", "required")
-		testutil.RequireValidationError(t, res.Errors, "username", "required")
-		testutil.RequireValidationError(t, res.Errors, "email", "required")
-		testutil.RequireValidationError(t, res.Errors, "password", "required")
+		t.Run("error: no required fields", func(t *testing.T) {
+			t.Parallel()
+
+			var (
+				req api.SetupRequest
+				res api.SetupResponse
+			)
+			sdk := tEnv.SDK()
+			httpRes, err := sdk.Setup(req, &res)
+			require.NoError(t, err, "error making request")
+
+			require.Equal(t, http.StatusBadRequest, httpRes.StatusCode)
+			testutil.RequireValidationError(t, res.Errors, "name", "required")
+			testutil.RequireValidationError(t, res.Errors, "username", "required")
+			testutil.RequireValidationError(t, res.Errors, "email", "required")
+			testutil.RequireValidationError(t, res.Errors, "password", "required")
+		})
+
+		t.Run("error: invalid email", func(t *testing.T) {
+			t.Parallel()
+
+			req := api.SetupRequest{
+				Name:     gofakeit.Name(),
+				Username: gofakeit.Username(),
+				Email:    "invalid-email",
+				Password: testutil.FakePassword(),
+			}
+			var res api.SetupResponse
+			sdk := tEnv.SDK()
+			httpRes, err := sdk.Setup(req, &res)
+			require.NoError(t, err, "error making request")
+
+			require.Equal(t, http.StatusBadRequest, httpRes.StatusCode)
+			testutil.RequireValidationError(t, res.Errors, "email", "email")
+		})
+
+		t.Run("error: invalid password", func(t *testing.T) {
+			t.Parallel()
+
+			req := api.SetupRequest{
+				Name:     gofakeit.Name(),
+				Username: gofakeit.Username(),
+				Email:    gofakeit.Email(),
+				Password: "no8char",
+			}
+
+			var res api.SetupResponse
+			sdk := tEnv.SDK()
+			httpRes, err := sdk.Setup(req, &res)
+			require.NoError(t, err, "error making request")
+
+			require.Equal(t, http.StatusBadRequest, httpRes.StatusCode)
+			testutil.RequireValidationError(t, res.Errors, "password", "min")
+		})
 	})
 
-	t.Run("valid email", func(t *testing.T) {
-		req := api.SetupRequest{
-			Name:     gofakeit.Name(),
-			Username: gofakeit.Username(),
-			Email:    "invalid-email",
-			Password: testutil.FakePassword(),
-		}
-		var res api.SetupResponse
+	t.Run("success", func(t *testing.T) {
+		t.Parallel()
+
+		tEnv := testutil.NewEnv(t)
+		tEnv.Start()
+		t.Cleanup(tEnv.Close)
 		sdk := tEnv.SDK()
-		httpRes, err := sdk.Setup(req, &res)
-		require.NoError(t, err, "error making request")
 
-		require.Equal(t, http.StatusBadRequest, httpRes.StatusCode)
-		testutil.RequireValidationError(t, res.Errors, "email", "email")
-	})
-
-	t.Run("valid password", func(t *testing.T) {
 		req := api.SetupRequest{
 			Name:     gofakeit.Name(),
 			Username: gofakeit.Username(),
 			Email:    gofakeit.Email(),
-			Password: "no8char",
+			Password: testutil.FakePassword(),
 		}
 
 		var res api.SetupResponse
-		sdk := tEnv.SDK()
 		httpRes, err := sdk.Setup(req, &res)
-		require.NoError(t, err, "error making request")
-
-		require.Equal(t, http.StatusBadRequest, httpRes.StatusCode)
-		testutil.RequireValidationError(t, res.Errors, "password", "min")
+		require.NoError(t, err, "error making the first request")
+		require.Equal(t, http.StatusOK, httpRes.StatusCode)
+		require.NotEmpty(t, res.Data.ID)
+		require.Equal(t, "admin", res.Data.Role)
 	})
-}
 
-func TestSetup_Success(t *testing.T) {
-	t.Parallel()
+	t.Run("error: setup can't run twice", func(t *testing.T) {
+		t.Parallel()
 
-	tEnv := testutil.NewEnv(t)
-	tEnv.Start()
-	t.Cleanup(tEnv.Close)
-	sdk := tEnv.SDK()
+		tEnv := testutil.NewEnv(t)
+		tEnv.Start()
+		t.Cleanup(tEnv.Close)
+		tEnv.Setup()
+		sdk := tEnv.SDK()
 
-	req := api.SetupRequest{
-		Name:     gofakeit.Name(),
-		Username: gofakeit.Username(),
-		Email:    gofakeit.Email(),
-		Password: testutil.FakePassword(),
-	}
+		req := api.SetupRequest{
+			Name:     gofakeit.Name(),
+			Username: gofakeit.Username(),
+			Email:    gofakeit.Email(),
+			Password: testutil.FakePassword(),
+		}
 
-	var res api.SetupResponse
-	httpRes, err := sdk.Setup(req, &res)
-	require.NoError(t, err, "error making the first request")
-	require.Equal(t, http.StatusOK, httpRes.StatusCode)
-	require.NotEmpty(t, res.Data.ID)
-	require.Equal(t, "admin", res.Data.Role)
-}
-
-func TestSetup_CantRunTwice(t *testing.T) {
-	t.Parallel()
-
-	tEnv := testutil.NewEnv(t)
-	tEnv.Start()
-	t.Cleanup(tEnv.Close)
-	tEnv.Setup()
-	sdk := tEnv.SDK()
-
-	req := api.SetupRequest{
-		Name:     gofakeit.Name(),
-		Username: gofakeit.Username(),
-		Email:    gofakeit.Email(),
-		Password: testutil.FakePassword(),
-	}
-
-	httpRes, err := sdk.Setup(req, nil)
-	require.NoError(t, err, "error making the first request")
-	require.Equal(t, http.StatusForbidden, httpRes.StatusCode)
-}
-
-func TestSetup_Login(t *testing.T) {
-	t.Parallel()
-
-	tEnv := testutil.NewEnv(t)
-	tEnv.Start()
-	t.Cleanup(tEnv.Close)
-	setup := tEnv.Setup()
-	sdk := tEnv.SDK()
-
-	var res api.LoginResponse
-	httpRes, err := sdk.Login(api.LoginRequest{
-		Email:    setup.Req().Email,
-		Password: setup.Req().Password,
-	}, &res)
-
-	require.NoError(t, err, "error making login request")
-	require.Equal(t, 200, httpRes.StatusCode, "unexpected status code")
+		httpRes, err := sdk.Setup(req, nil)
+		require.NoError(t, err, "error making the first request")
+		require.Equal(t, http.StatusForbidden, httpRes.StatusCode)
+	})
 }
